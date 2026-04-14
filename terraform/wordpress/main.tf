6e80b7797cec1b1d443f944dd9c66a212ba63277
JoePearson93 <joe.pearson@live.com>
2026-04-14 21:04:08 +0100

    init: wordpress terraform project

--- terraform/wordpress/main.tf
+++ terraform/wordpress/main.tf
@@ -2,7 +2,18 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.40.0"
+       version = "6.40.0"
     }
   }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "wordpress" {
+  ami                     = "ami-0324bce2436ce02b2"
+  instance_type           = "t3.micro"
 }
