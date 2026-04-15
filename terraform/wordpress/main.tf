5723e80063cfb97d74d5cd990bb569f5582c7e77
JoePearson93 <joe.pearson@live.com>
2026-04-15 19:02:36 +0100

    add IAM role and instance profile for EC2 access

--- terraform/wordpress/main.tf
+++ terraform/wordpress/main.tf
@@ -13,18 +13,42 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_instance" "wordpress" {
+resource "aws_instance" "wordpress"{
   ami                     = "ami-0324bce2436ce02b2"
   instance_type           = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.wordpress_sg.id]
-  key_name = aws_key_pair.wordpress_keypair.key_name
+  vpc_security_group_ids  = [aws_security_group.wordpress_sg.id]
+  key_name                = aws_key_pair.wordpress_keypair.key_name
+  associate_public_ip_address = true
+  iam_instance_profile        =  aws_iam_instance_profile.wordpress_profile.name
 
-  tags = {
-    Name        = "wordpress-ec2"
-    Project     = "wordpress"
-    Environment = "dev"
-    ManagedBy   = "terraform"
+  # user_data = templatefile("${path.module}/user_data.sh", {
+  #   db_pass = var.database_password
+  # })
+ }
+
+data "aws_iam_policy_document" "wordpress_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
   }
+}
+
+resource "aws_iam_role" "wordpress_role" {
+  name = "wordpress-role"
+  assume_role_policy = data.aws_iam_policy_document.wordpress_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "wordpress_ssm" {
+  role = aws_iam_role.wordpress_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+resource "aws_iam_instance_profile" "wordpress_profile" {
+  name = "wordpress-profile"
+  role = aws_iam_role.wordpress_role.name
 }
 
 resource "aws_key_pair" "wordpress_keypair" {
